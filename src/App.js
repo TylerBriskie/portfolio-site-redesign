c2a0489ab1388c725e4d9a7175f5f0793f44529e
Tyler Briskie <tylerbriskie@gmail.com>
2019-01-05 16:56:57 -0700

    react router added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import Header from './components/Header/Header';
-import logo from './logo.svg';
 import './App.scss';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
+const Index = () => <h2>Home</h2>;
+const Music = () => <h2>Music Stuff Goes here</h2>;
+const Resume = () => <h2>Stunning online resume</h2>;
+const Arcade = () => <h2>Welcome to the TylerBriskie.com Arcade and casino!</h2>;
+
+
+const App = () => (
+    <div className="App">
         <Header />
-      </div>
-    );
-  }
-}
+        <Router>
+          <div>
+            <Route path="/" exact component={Index} />
+            <Route path="/music/" component={Music} />
+            <Route path="/resume/" component={Resume} />
+            <Route path="/arcade/" component={Arcade} />
+          </div>
+        </Router>
+    </div>
+);
 
 export default App;
+
