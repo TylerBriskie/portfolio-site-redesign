f7bc5a691193117d1527803bcbb4ed268788c5f2
Tyler Briskie <tylerbriskie@gmail.com>
2019-01-07 20:27:35 -0700

    image carousel added to music page

--- src/components/Music/Music.jsx
+++ src/components/Music/Music.jsx
@@ -1,5 +1,12 @@
 import React, { Component} from 'react';
+import { Carousel } from 'react-responsive-carousel';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 import './Music.scss';
+import photo_01 from "../../assets/music_01.jpg";
+import photo_02 from "../../assets/music_02.jpg";
+import photo_03 from "../../assets/music_03.jpg";
+import photo_04 from "../../assets/music_04.jpg";
+
 
 class Music extends Component {
     componentDidMount(){
@@ -7,7 +14,30 @@ class Music extends Component {
     }
     render(){
         return (
-            <h1>Music page, coming soon...</h1>
+            <div id="music-page-container">
+                <Carousel dynamicHeight={true}
+                          showArrows={false}
+                          showIndicators={false}
+                          showStatus={false}
+                          infiniteLoop={true}
+                          width="50vw"
+                          emulateTouch={true}
+                          showThumbs={false}
+                          autoPlay={true}
+                          interval={5000}
+                          transitionTime={500}
+                          className="carousel-container">
+                    <div className="carousel-image">
+                        <img src={photo_01} />
+                    </div>
+                    <div className="carousel-image">
+                        <img src={photo_02} />
+                    </div>
+                    <div className="carousel-image">
+                        <img src={photo_03} />
+                    </div>
+                </Carousel>
+            </div>
         );
     }
 
