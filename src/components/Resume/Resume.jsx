61b0088c951901a8624e0918c208ae846d3d28c4
Briskie,Tyler <tylerbriskie@gmail.com>
2019-02-01 11:40:16 -0700

    resume page mocked up

--- src/components/Resume/Resume.jsx
+++ src/components/Resume/Resume.jsx
@@ -4,7 +4,31 @@ import './Resume.scss';
 class Resume extends Component {
     render(){
         return (
-            <h1>Online Resume</h1>
+          <div className="resume-page-container">
+              <article className="scrollable">
+                  <section className="experience">
+                      <header>
+                          <h1>Vertafore <span>2017 - Present</span></h1>
+                      </header>
+                      <p>Here's stuff about vertafore</p>
+                  </section>
+                  <section className="experience">
+                      <header>
+                          <h1>Galvanize Web Development Immersive Bootcamp <span>2016-2017</span></h1>
+                      </header>
+                  </section>
+              </article>
+              <aside id="skills-and-proficiencies">
+                  <h1>Skills and Proficiencies</h1>
+                  <div id="skill-table">
+                      <div className="skill-single">
+                          <h3>Java</h3>
+                          <div className="skill-level 8"></div>
+                      </div>
+                  </div>
+              </aside>
+          </div>
+
         );
     }
 
